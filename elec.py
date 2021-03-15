cb59d2d8b001acd584dcdbbc567009fb79422b6a
Parasmanithakur <66260872+Parasmanithakur@users.noreply.github.com>
2021-03-15 22:33:56 +0530

    Add files via upload

--- elec.py
+++ elec.py
@@ -1,6 +1,6 @@
 
 
-
+from matplotlib import pyplot as plt
 maxdemand =float(input("Enter Max Demand "))
 
     
@@ -12,8 +12,9 @@ CF=[]
 temp=minLf
 
 while temp<1:
-    temp=temp+0.01
+    
     LF.append(temp)
+    temp=temp+0.01
 
 
 temp=0.85
@@ -21,7 +22,9 @@ while temp<1:
      CF.append(temp)
      temp=temp+0.05
 
-
+cf1=[]
+cf2=[]
+cf3=[]
 def fun():
     
   
@@ -31,10 +34,26 @@ def fun():
         units=maxdemand*lf*8760
         print("Units "+str(units) +"KWH")
         for cf in CF:
-           
+
+            if cf==0.85:
+               cf1.append(tarif*units/(maxdemand/cf))
+            if cf==0.90:
+               cf2.append(tarif*units/(maxdemand/cf))   
+            if ("%.2f" % cf)=='0.95':
+               cf3.append(tarif*units/(maxdemand/cf))             
             print("  power Factor" +str("%.2f" % cf))
             
             print("    -Rs/kva"+ str("%.2f" % (tarif*units/(maxdemand/cf))))
                        
 fun()
+
+plt.plot(LF,cf1,label='pf-0.85')
+plt.plot(LF,cf2,label='pf-0.90')
+plt.plot(LF,cf3,label='pf-0.95')
+plt.ylabel('Tarif (Rs/kva)')
+plt.xlabel('Load factor')
+plt.title('Tarif by LF')
+plt.legend()
+plt.show()
+
     
